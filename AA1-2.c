15612d2281c9e3e4f126b2e0e57af808eff70e0c
gmsantos <gabriel.ms1@hotmail.com>
2020-05-04 01:38:03 -0300

    improve fibonacci code evaluation

--- AA1-2.c
+++ AA1-2.c
@@ -4,14 +4,51 @@
  Author      : Gabriel Machado
  Description : Discussão sobre algoritmos recursivos
  ============================================================================
- */
+ 
+ Analise do código 
+
+ Aqui temos a implementação da sequencia de Fibonacci utilizando uma 
+ abordagem recursiva e outra iterativa.
+
+ Na abordagem iterativa, precisamos uma quantidade maior de código para
+ implementar a sequencia de fibonacci até o termo necessário. A vantagem
+ dessa abordagem é uma implementação mais simples e não requer o entendimento
+ de recursão.
+
+ Na abordagem recursiva, temos uma menor quantidade de código executado,
+ pois a função consiste em executar a mesma função com os dois termos anteriores
+ até atingir a codição de parada. Isso resulta num grande número de chamadas
+ da mesma função, o que é menos eficiente que a abordagem interativa e
+ pode causar um estouro de pilha em termos em maiores termos da sequencia.
+ Além disso, o código não é tão claro e simples se comparado com a 
+ abordagem iterativa.
+
+ Nesse caso a abordagem iterativa se mostra mais eficiente. Abordagens recursivas
+ geralmente tem uma complexidade maior de serem resolvidos e para casos
+ que podem ser resolvidos aplicando um único laço de repetição, como
+ Fibonacci e cálculos fatorias, seu uso pode ser. Podemos avaliar sua eficiência 
+ com o exemplo de execução com o termo 30 da sequencia:
+ 
+ Entre com o termo da sequencia desejado:30
+
+ Fibonacci Iterativo:
+ 832040 
+ 
+ Executado em: 0.000006 segundos
+ 
+ Fibonacci Recursivo:
+ 832040 
+
+ Executado em: 0.005638 segundos
+
+*/
 
 #include <stdio.h>
 #include <time.h>
 
 // Definição dos métodos
-// n -> enésimo termo na sequencia de Fibonacci 
-int fibonacci_interactive(int n);
+// n -> enésimo termo na sequencia de Fibonacci
+int fibonacci_iteractive(int n);
 int fibonacci_recursive(int n);
 
 // A função principal executa ambas implementação de Fibonacci,
@@ -26,11 +63,13 @@ int main()
     printf("Entre com o termo da sequencia desejado:");
     scanf("%d", &n);
 
+    // Executa a abordagem iterativa e mede seu tempo de execução
     printf("\nFibonacci Iterativo:\n");
     start = clock();
-    printf("%d ", fibonacci_interactive(n));
+    printf("%d ", fibonacci_iteractive(n));
     printf("\n\nExecutado em: %f segundos\n", (double)(clock() - start) / CLOCKS_PER_SEC);
 
+    // Executa a abordagem recursiva e mede seu tempo de execução
     printf("\nFibonacci Recursivo:\n");
     start = clock();
     printf("%d ", fibonacci_recursive(n));
@@ -40,23 +79,24 @@ int main()
 }
 
 // Algoritmo Iterativo
-//
-// Contras: Código mais longo
-int fibonacci_interactive(int n)
+int fibonacci_iteractive(int n)
 {
     int i;
     int termo_anterior = 0; // Termos iniciais da sequencia de fibonacci
     int termo_atual = 1;
     int proximo_termo; // incializa com o primeiro termo
 
-    if (n == 0) {
+    if (n == 0)
+    {
         return termo_anterior; // Retorna 0 se enviado a posição zero
     }
 
-    for (i = 2; i <= n; i++) // começa a contagem em 2, para n = 1 retornar 
+    for (i = 2; i <= n; i++) // começa a contagem em 2, para n = 1 retornar
     {
-        proximo_termo = termo_anterior + termo_atual; // o termo seguinte é a soma dos dois termos anteriores
-        termo_anterior = termo_atual; // troca os termos anteriores para a proxima interação
+        // o termo seguinte é a soma dos dois termos anteriores
+        proximo_termo = termo_anterior + termo_atual;
+        // troca os termos anteriores para a proxima interação
+        termo_anterior = termo_atual;
         termo_atual = proximo_termo;
     }
 
@@ -64,26 +104,13 @@ int fibonacci_interactive(int n)
 }
 
 // Algoritmo Recursivo
-// Prós: código mais curto
-//
-// Contras: Maior uso de memória
-// Requer um ponto de parada (return == 0 e return == 1)
-// Complexidade algorimica maior O(2^n)
 int fibonacci_recursive(int n)
 {
-    if (n < 2) {
+    if (n < 2)
+    { // definição do critério de parada
         return n;
     }
 
-    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2); // Soma recursivamente 
+    // Soma recursivamente os termos anteriores
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
-
-// Conclusão
-//
-// Nesse caso não vejo uma justificativa em implementar o algorítimo usando recursão.
-// Algoritomos que podem ser resolvidos aplicando um único laço de repetição, como
-// Fibocacci e cálculos fatorias tem uma complexidade algorítmica maior se resolvidos
-// utilizando.
-//
-// A recursão se mostra mais eficiente com estruturas mais complexas, como o caso
-// de um algoritmo de busca binaria.
